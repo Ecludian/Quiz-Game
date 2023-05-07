fcf7a2c79bc16cb5639aee9648670cdd86c0f31b
Charel Adhi Nugroho <charelnugroho@gmail.com>
2023-05-07 15:04:58 +0700

    basic true or false
    done
    need scoring system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Question[] questions;
+    private static List<Question> unansweredQuestion;
+
+    public Button correctButton;
+    public Button wrongButton;
+
+    private Question currentQuestion;
+    //private Question.AnswerList userAnswer;
+    [SerializeField]
+    private TMP_Text questionText, textA, textB, textC, textD;
+    [SerializeField]
+    private Image questionPic;
+
+    /*  [SerializeField]
+      private TMP_Text correctAnswerText, wrongAnswerText;*/
+
+
+    [SerializeField]
+    private float timeBetweenQuestion = 1.5f;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private int score;
+  
+
+    private void Start()
+    {
+       /* Button corrButton = correctButton.GetComponent<Button>();
+        Button wroButton = wrongButton.GetComponent<Button>();*/
+
+        if (unansweredQuestion == null || unansweredQuestion.Count == 0)
+        {
+            unansweredQuestion = questions.ToList<Question>();
+        }
+
+        SetCurrentQuestion();
+
+    }
+
+    void SetCurrentQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestion.Count);
+        currentQuestion = unansweredQuestion[randomQuestionIndex];
+
+        questionText.text = currentQuestion.question;
+        questionPic.sprite = currentQuestion.image;
+     
+        if (unansweredQuestion.Count == 0)
+        {
+          
+        }
+
+        /*textA.text = currentQuestion.answerA;
+        textB.text = currentQuestion.answerB;
+        textC.text = currentQuestion.answerC;
+        textD.text = currentQuestion.answerD;*/
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestion.Remove(currentQuestion);
+       
+        yield return new WaitForSeconds(1);
+        animator.SetTrigger("Reset");
+        yield return new WaitForSeconds(timeBetweenQuestion);
+        SetCurrentQuestion();
+        StartCoroutine(EnabledButton());
+
+    }
+
+    IEnumerator EnabledButton()
+    {
+        yield return new WaitForSeconds(0.2f);
+        correctButton.enabled = true;
+        wrongButton.enabled = true;
+    }
+
+    //use enum for mutliple choice
+    /*  public void UserAnswer()
+      {
+         if(userAnswer == Question.AnswerList.A)
+          {
+
+          }
+          if (userAnswer == Question.AnswerList.B)
+          {
+
+          }
+          if (userAnswer == Question.AnswerList.C)
+          {
+
+          }
+          if (userAnswer == Question.AnswerList.D)
+          {
+
+          }
+      }
+  */
+
+
+    public void UserSelectCorrectAnswer()
+    {
+        correctButton.enabled = false;
+        wrongButton.enabled = false;
+        if (currentQuestion.isCorrect)
+        {
+            Debug.Log("Correct!");
+            animator.SetTrigger("True");
+            score = +10;
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+            animator.SetTrigger("False");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+    public void UserSelectWrongAnswer()
+    {
+        correctButton.enabled = false;
+        wrongButton.enabled = false;
+        if (!currentQuestion.isCorrect)
+        {
+            Debug.Log("Correct!");
+            animator.SetTrigger("False");
+            score = +10;
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+            animator.SetTrigger("True");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class Question
+{
+    public string question;
+    public Sprite image;
+    public bool isCorrect;
+    /*public string answerA;
+    public string answerB;
+    public string answerC;
+    public string answerD;*/
+
+//public enum AnswerList { A, B, C, D };
+}
